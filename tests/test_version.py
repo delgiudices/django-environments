1f86698eabe5dc7b5b6b15ac88432db2010879c0
Luis Del Giudice <luis.dg19@gmail.com>
2016-05-15 00:44:56 -0400

    Fixing tests

--- tests/test_version.py
+++ tests/test_version.py
@@ -8,12 +8,10 @@
 # http://www.opensource.org/licenses/MIT-license
 # Copyright (c) 2016, Luis Del Giudice <luis.dg19@gmail.com>
 
-from preggy import expect
-
 from django_environments import __version__
 from tests.base import TestCase
 
 
 class VersionTestCase(TestCase):
     def test_has_proper_version(self):
-        expect(__version__).to_equal('0.1.0')
+        self.assertEqual(__version__, '0.1.0')
